5262862bbd1498a002dfbbaba64f367ea96562f2
Eclipse <Eclipse@something.com>
2012-03-20 23:46:51 +0100

    implementovan LinearMovementControl

--- Application/Scene/Controls/LinearMovementControl.cs
+++ Application/Scene/Controls/LinearMovementControl.cs
@@ -1,20 +1,32 @@
 using System;
+using System.Windows;
 using Orbit.Scene.Entities;
 
 namespace Orbit.Scene.Controls
 {
     public class LinearMovementControl : Control
     {
+        private IMovable me;
+
         public float Speed { get; set; }
 
-        public override void InitControl(ISceneObject me)
+        public override void InitControl(ISceneObject obj)
         {
-            throw new Exception("Not implemented");
+            if (!(me is IMovable))
+            {
+                Console.Error.WriteLine("LinearMovementControl cannot be attached to non-movable object!");
+                return;
+            }
+
+            me = (IMovable)obj;
         }
 
         public override void UpdateControl(float tpf)
         {
-            throw new Exception("Not implemented");
+            if (me == null)
+                return;
+
+            ((ISceneObject)me).SetPosition(((ISceneObject)me).GetPosition() + (me.GetDirection() * Speed));
         }
     }
 }
