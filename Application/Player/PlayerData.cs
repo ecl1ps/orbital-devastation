7ef37ff4994202607e2a0b6a4fa45564d15439ac
Eclipse <Eclipse@something.com>
2012-03-20 23:45:22 +0100

    implementace PlayerData a pridani tridy se sdilenymi definicemi

---
+++ Application/Player/PlayerData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Orbit.Scene.Entities;
+
+namespace Orbit.Player
+{
+    class PlayerData : IPlayerData
+    {
+        private Base myBase;
+        private int score;
+
+        public PlayerData()
+        {
+            score = 0;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public void UpdateScore(int amount)
+        {
+            score += amount;
+        }
+
+        public int GetBaseIntegrity()
+        {
+            return myBase.Integrity;
+        }
+
+        public void UpdateBaseIntegrity(int amount)
+        {
+            myBase.Integrity += amount;
+        }
+
+        public PlayerPosition GetPosition()
+        {
+            return myBase.BasePosition;
+        }
+
+        public void SetBase(Base newBase) 
+        {
+            myBase = newBase;
+        }
+    }
+}
---
+++ Application/SharedDef.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orbit
+{
+    class SharedDef
+    {
+        public const int MIN_SPHERE_RADIUS          = 10;
+        public const int MAX_SPHERE_RADIUS          = 30;
+
+        public const int MIN_SPHERE_SPEED           = 2;
+        public const int MAX_SPHERE_SPEED           = 5;
+    }
+}
