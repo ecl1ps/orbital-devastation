3ab961e4d714c9d32cb09935abe0f5e0f2b02e73
Eclipse <Eclipse@something.com>
2012-04-02 17:51:52 +0200

    pridani gui a prepsani hlavni tridy

---
+++ Application/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Navigation;
+using Orbit.Core.Scene;
+using Orbit.Gui;
+using System.Threading;
+using System.ComponentModel;
+
+namespace Orbit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private SceneMgr sceneMgr;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run(new GameWindow());
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            StartHostedGame();
+            StartGameThread();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            sceneMgr.RequestStop();
+        }
+
+        public void InitScene()
+        {
+            GameWindow wnd = MainWindow as GameWindow;
+            sceneMgr = new SceneMgr(true);
+            sceneMgr.SetCanvas(wnd.mainCanvas);
+            sceneMgr.Init();
+        }
+
+        public void StartHostedGame()
+        {
+            InitScene();
+        }
+
+        public void ConnectToGame()
+        {
+            throw new Exception("Not implemented");
+        }
+
+        public void StartGameThread()
+        {
+            Thread gameThread = new Thread(new ThreadStart(sceneMgr.Run));
+            gameThread.Start();
+        }
+    }
+}
---
+++ Application/Gui/GameWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orbit.Gui
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    Application.Current.Shutdown();
+                    break;
+            }
+        }
+    }
+}
