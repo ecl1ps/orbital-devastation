f86a0c6233e0a44c231b7d0c4b9c5fa1984775c0
Eclipse <Eclipse@something.com>
2012-04-03 22:18:01 +0200

    Collision helper pro dve kruznice a kruznici a obdelnik

---
+++ Application/Core/CollisionHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Orbit.Core
+{
+    class CollisionHelper
+    {
+        private static Vector tempVector = new Vector();
+
+        public static bool intersectsCircleAndCircle(Vector center1, int radius1, Vector center2, int radius2)
+        {
+            return (center1 - center2).Length <= radius1 + radius2;
+        }
+
+        public static bool intersectsCircleAndSquare(Vector circleCenter, int circleRadius, Vector rectPosition, Size rectSize)
+        {
+
+            tempVector.X = Math.Abs(circleCenter.X - rectPosition.X - rectSize.Width / 2);
+            tempVector.Y = Math.Abs(circleCenter.Y - rectPosition.Y - rectSize.Height / 2);
+
+            if (tempVector.X > (rectSize.Width / 2 + circleRadius))
+                return false;
+
+            if (tempVector.Y > (rectSize.Height / 2 + circleRadius))
+                return false;
+
+            if (tempVector.X <= (rectSize.Width / 2))
+                return true;
+
+            if (tempVector.Y <= (rectSize.Height / 2))
+                return true;
+
+            double cornerDistanceSquared = Math.Pow((tempVector.X - rectSize.Width / 2), 2) +
+                                 Math.Pow((tempVector.Y - rectSize.Height / 2), 2);
+
+            return cornerDistanceSquared <= Math.Pow(circleRadius, 2);
+        }
+    }
+}
