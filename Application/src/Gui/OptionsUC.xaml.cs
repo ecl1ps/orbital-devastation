530266eaefee94c39c0f7282ae07988b51a8d47e
Eclipse <Eclipse@something.com>
2012-04-12 01:39:14 +0200

    nove gui pro hlavni menu hry

---
+++ Application/src/Gui/OptionsUC.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orbit.Gui
+{
+    /// <summary>
+    /// Interaction logic for OptionsUC.xaml
+    /// </summary>
+    public partial class OptionsUC : UserControl
+    {
+        public OptionsUC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSinglePlayer_Click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current as App).StartGame();
+        }
+
+        private void btnHostNewGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFindHostedGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
