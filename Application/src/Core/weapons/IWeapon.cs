25c58e0091b167ddb1b4c12b0548e0389009bd10
hromada2 <jakub.hromadka@gmail.com>
2012-06-01 00:28:35 +0200

    neimplementovana iweapon

---
+++ Application/src/Core/weapons/IWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orbit.Core.Scene.Entities;
+
+namespace Orbit.src.Core.weapons
+{
+    interface IWeapon
+    {
+        ISceneObject bullet { get; set; }
+
+    }
+}
