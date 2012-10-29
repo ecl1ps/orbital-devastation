ecl1ps/orbital-devastation

commit 9e6daf3f237542809bfcef2e92b535f1572ba0be

getSceneObject(type) nyní správně vrací i potomky

Application/Core/Client/SceneMgr.cs
@@ -624,8 +624,7 @@ namespace Orbit.Core.Client
         {
             Application.Current.Dispatcher.BeginInvoke(new Action(() =>
             {
-                (Application.Current as App).GameEnded();
-                //(Application.Current as App).ExitGame();
+                (Application.Current as App).ExitGame();
             }));
         }
 
@@ -754,7 +753,7 @@ namespace Orbit.Core.Client
 
             foreach (ISceneObject obj in objects) 
             {
-                if (obj.GetType() == clazz)
+                if (obj.GetType().IsAssignableFrom(clazz))
                     temp.Add(obj);
             }
 

Application/Application.csproj
@@ -139,6 +139,7 @@
     <Compile Include="Core\Players\ScoreDefines.cs" />
     <Compile Include="Core\Players\StatsMgr.cs" />
     <Compile Include="Core\Properties.cs" />
+    <Compile Include="Core\Scene\Controls\Implementations\ControlableDeviceControl.cs" />
     <Compile Include="Core\Scene\Controls\Implementations\LaserDamageControl.cs" />
     <Compile Include="Core\Scene\Controls\Implementations\MiningBotControl.cs" />
     <Compile Include="Core\Scene\Controls\Implementations\StretchingLineControl.cs" />
@@ -150,6 +151,7 @@
     <Compile Include="Core\Scene\Controls\Implementations\LinearRotationControl.cs" />
     <Compile Include="Core\Scene\Entities\ICatchable.cs" />
     <Compile Include="Core\Scene\Entities\IControledDevice.cs" />
+    <Compile Include="Core\Scene\Entities\Implementations\MiningModule.cs" />
     <Compile Include="Core\Scene\Entities\Implementations\SimpleLine.cs" />
     <Compile Include="Core\Scene\Entities\Implementations\SingularityBouncingBullet.cs" />
     <Compile Include="Core\Scene\Entities\Implementations\Circle.cs" />

commit 3e4d7992207890e3a7d04fd3d345ff2eff28a329

fixed end game broken in 9e6daf3f237542809bfcef2e92b535f1572ba0be

Application/Core/Client/SceneMgr.cs
@@ -624,7 +624,8 @@ namespace Orbit.Core.Client
         {
             Application.Current.Dispatcher.BeginInvoke(new Action(() =>
             {
-                (Application.Current as App).ExitGame();
+                (Application.Current as App).GameEnded();
+                //(Application.Current as App).ExitGame();
             }));
         }
 