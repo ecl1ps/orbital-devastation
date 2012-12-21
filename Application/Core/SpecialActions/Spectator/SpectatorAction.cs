f08671ea416cb920205bc3aa07b7e0784535f69a
hromada2 <jakub.hromadka@gmail.com>
2012-12-21 17:05:59 +0100

    interface a implementace spectator akce

---
+++ Application/Core/SpecialActions/Spectator/SpectatorAction.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orbit.Core.Client;
+using Orbit.Core.Scene.Controls.Implementations;
+using Orbit.Core.Scene.Entities.Implementations;
+
+namespace Orbit.Core.SpecialActions.Spectator
+{
+   public enum Limit{
+        EXACT,
+        TOP_LIMIT,
+        BOTTOM_LIMIT
+    }
+
+    public abstract class SpectatorAction : SpecialAction, ISpectatorAction
+    {
+        protected MiningModuleControl control;
+
+        protected int normal;
+        protected int gold;
+        protected Limit limit;
+
+
+        public SpectatorAction(MiningModuleControl control, SceneMgr mgr, Players.Player owner)
+            : base(mgr, owner)
+        {
+            this.control = control;
+        }
+
+        public float Percentage
+        {
+            get { return computePercentage(); }
+        }
+
+        private float computePercentage()
+        {
+            List<MiningObject> list = control.currentlyMining;
+
+            int normal = 0;
+            int gold = 0;
+
+            foreach (MiningObject obj in list)
+                if (obj.Obj is Asteroid)
+                {
+                    if ((obj.Obj as Asteroid).AsteroidType == AsteroidType.GOLDEN)
+                        gold++;
+                    else if ((obj.Obj as Asteroid).AsteroidType == AsteroidType.NORMAL)
+                        normal++;
+                }
+
+            switch (limit)
+            {
+                case Limit.EXACT:
+                    return computePercentageExact(normal, gold);
+                
+                case Limit.BOTTOM_LIMIT:
+                    return computePercentageBottom(normal, gold);
+
+                case Limit.TOP_LIMIT:
+                    return computePercentageTop(normal, gold);
+                
+                default:
+                    throw new Exception("Unknown limit value");
+            }
+        }
+
+        protected float computePercentageTop(int normal, int gold)
+        {
+            float normPerc = this.normal == 0 ? 1 : 0;
+            float goldPerc = this.gold == 0 ? 1 : 0;
+
+            if (normPerc != 1)
+                normPerc = normal / this.normal;
+
+            if (goldPerc != 1)
+                goldPerc = gold / this.gold;
+
+            if (goldPerc < 1)
+                goldPerc = 1;
+            else
+                goldPerc = computeModulo(goldPerc);
+
+            if (normPerc < 1)
+                normPerc = 1;
+            else
+                normPerc = computeModulo(normPerc);
+
+            return goldPerc * normPerc;
+        }
+
+        protected float computePercentageBottom(int normal, int gold)
+        {
+            float normPerc = this.normal == 0 ? 1 : 0;
+            float goldPerc = this.gold == 0 ? 1 : 0;
+
+            if (normPerc != 1)
+                normPerc = normal / this.normal;
+
+            if (goldPerc != 1)
+                goldPerc = gold / this.gold;
+
+            if (goldPerc > 1)
+                goldPerc = 1;
+            else
+                goldPerc = computeModulo(goldPerc);
+
+            if (normPerc > 1)
+                normPerc = 1;
+            else
+                normPerc = computeModulo(normPerc);
+
+            return goldPerc * normPerc;
+        }
+
+        protected float computePercentageExact(int normal, int gold)
+        {
+
+            float normPerc = this.normal == 0 ? 1 : 0;
+            float goldPerc = this.gold == 0 ? 1 : 0;
+
+            if (normPerc != 1)
+                normPerc = normal / this.normal;
+
+            if (goldPerc != 1)
+                goldPerc = gold / this.gold;
+
+            if (goldPerc > 1)
+                goldPerc = 1 - computeModulo(goldPerc);
+            else
+                goldPerc = computeModulo(goldPerc);
+
+            if (normPerc > 1)
+                normPerc = 1 - computeModulo(normPerc);
+            else
+                normPerc = computeModulo(normPerc);
+
+            return goldPerc * normPerc;
+        }
+
+        private float computeModulo(float perc)
+        {
+            return ((perc * 100) % 100) / 100;
+        }
+    }
+}
