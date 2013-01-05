962717a907bb243d9289836ae966b20d86ca1fa7
Eclipse <Eclipse@something.com>
2013-01-05 03:36:53 +0100

    mozny crashfix ve floating text manageru

--- Application/Core/Client/GameStates/FloatingTextManager.cs
+++ Application/Core/Client/GameStates/FloatingTextManager.cs
@@ -50,8 +50,8 @@ namespace Orbit.Core.Client.GameStates
                     GrowFloatingText(floatingTexts[i]);
             }
 
-            foreach (FloatingText ft in newFloatingTexts)
-                CreateFloatingText(ft);
+            for (int i = 0; i < newFloatingTexts.Count; ++i)
+                CreateFloatingText(newFloatingTexts[i]);
 
             newFloatingTexts.Clear();
         }
