bfdc985ba6fe3cd8d0bb180afb9efcb3b170460d
hromada2 <jakub.hromadka@gmail.com>
2013-01-06 03:03:51 +0100

    rozpracovany allert message manager

---
+++ Application/Core/Client/GameStates/AllertMessageManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orbit.Gui;
+
+namespace Orbit.Core.Client.GameStates
+{
+    public class AlertMessage 
+    {
+        public String Text { get; set; }
+        public float Time { get; set; }
+
+        public AlertMessage(String text, float time)
+        {
+            Text = text;
+            Time = time;
+        }
+    }
+
+    public class AlertMessageManager : IGameState
+    {
+        public float AnimationTime { get; set; }
+
+        private float timer = 0;
+        private AlertBox element = null;
+
+        private bool opening = false;
+        private bool closing = false;
+        private bool showing = false;
+        private bool ready = true;
+
+        private SceneMgr mgr;
+        private List<AlertMessage> messages;
+
+        public AlertMessageManager(SceneMgr mgr, float animationTime)
+        {
+            this.mgr = mgr;
+            this.messages = new List<AlertMessage>();
+            AnimationTime = animationTime;
+        }
+
+        public void Update(float tpf)
+        {
+            if (ready && messages.Count > 0)
+                Open(tpf);
+            else if (opening)
+                Open(tpf);
+            else if (showing)
+                Display(tpf);
+            else if (closing)
+                Close(tpf);
+        }
+
+        private void Open(float tpf)
+        {
+            if (ready && !opening)
+            {
+                timer = AnimationTime;
+                ready = false;
+                opening = true;
+            }
+
+            if (timer <= 0)
+            {
+                opening = false;
+                Display(tpf);
+                return;
+            }
+
+            timer -= tpf;
+            Animate(true);
+        }
+
+        private void Display(float tpf)
+        {
+            if (!showing)
+            {
+                showing = true;
+                timer = getFirstItem().Time;
+            }
+
+            if (timer <= 0)
+            {
+                showing = false;
+                Close(tpf);
+                return;
+            }
+
+            timer -= tpf;
+        }
+
+        private void Close(float tpf)
+        {
+            if (!closing)
+            {
+                closing = true;
+                timer = AnimationTime;
+            }
+
+            if (timer <= 0)
+            {
+                closing = false;
+                ready = true;
+                return;
+            }
+
+            timer -= tpf;
+            Animate(false);
+        }
+
+        private void Animate(bool open)
+        {
+            throw new NotImplementedException();
+        }
+
+        private AlertMessage getFirstItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show(String text, float time)
+        {
+            messages.Add(new AlertMessage(text, time));
+        }
+    }
+}
---
+++ Application/Gui/AlertBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Orbit.Core;
+
+namespace Orbit.Gui
+{
+    /// <summary>
+    /// Interaction logic for AlertBox.xaml
+    /// </summary>
+    public partial class AlertBox : UserControl
+    {
+        private double widthLeft;
+        private double widthRight;
+
+        public AlertBox()
+        {
+            InitializeComponent();
+            widthLeft = DoorLeft.Width;
+            widthRight = DoorRight.Width;
+        }
+
+        public void OpenDoors(double val)
+        {
+            DoorLeft.Width = FastMath.LinearInterpolate(widthLeft, 0, val);
+        }
+
+        public void Hide(bool hide)
+        {
+            if (hide)
+                this.Visibility = System.Windows.Visibility.Hidden;
+            else
+                this.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
