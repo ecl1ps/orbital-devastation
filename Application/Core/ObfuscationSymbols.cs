f4c02663c6070579c97a16dcc53684f7578f9a19
Eclipse <Eclipse@something.com>
2013-01-10 11:51:26 +0100

    nastaveni mergingu a embeddingu pro obfuscator - ted se vytvari jen jedna binarka a zbytek je v ni

--- Application/Core/ObfuscationSymbols.cs
+++ Application/Core/ObfuscationSymbols.cs
@@ -3,3 +3,19 @@ using System.Reflection;
 
 [assembly: Obfuscation(Feature = "encrypt symbol names with password ks5n!f68H-7Lkqw", Exclude = false)]
 [assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
+
+// merging a embedding jsou dva zpusoby jak z vice resources udelat jednu assembly, navic pridava do assembly encryption a compression
+// merge - vykonnejsi, rychlejsi startup, funguje i v omezenych prostredich (widows phone etc.), obcas nejde pouzit
+[assembly: Obfuscation(Feature = "merge with Lidgren.Network.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "merge with MB.Tools.dll", Exclude = false)]
+
+// embeding jen vlozi resources do do jednoho assembly - pomalejsi ale nerozbije aplikaci
+[assembly: Obfuscation(Feature = "embed irrKlang.NET4.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "embed ShaderEffectLibrary.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "embed log4net.dll", Exclude = false)]
+
+//[assembly: Obfuscation(Feature = "encrypt resources logger.config", Exclude = false)]
+
+// zapnuti debugovani v release
+//[assembly: Obfuscation(Feature = "debug", Exclude = false)]
+//[assembly: Obfuscation(Feature = "debug renaming", Exclude = false)]
