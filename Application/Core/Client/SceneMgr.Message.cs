3345071adbd6489134bd4e62a9723de7806448f7
Eclipse <Eclipse@something.com>
2013-01-20 23:18:42 +0100

    odstraneni zbytecnych parametru

--- Application/Core/Client/SceneMgr.Message.cs
+++ Application/Core/Client/SceneMgr.Message.cs
@@ -351,7 +351,7 @@ namespace Orbit.Core.Client
                 if (p.IsActivePlayer())
                 {
                     inputMgr = new PlayerInputMgr(p, this, actionBarMgr);
-                    actionBarMgr.CreateActionBarItems(p.GetActions<IPlayerAction>(this), false);
+                    actionBarMgr.CreateActionBarItems(p.GetActions<IPlayerAction>(), false);
                 }
                 else
                 {
@@ -360,7 +360,7 @@ namespace Orbit.Core.Client
                     p.Device.AddControl(mc);
 
                     inputMgr = new SpectatorInputMgr(p, this, p.Device, actionBarMgr);
-                    actionBarMgr.CreateActionBarItems(p.GetActions<ISpectatorAction>(this), true);
+                    actionBarMgr.CreateActionBarItems(p.GetActions<ISpectatorAction>(), true);
                 }
             }
         }
