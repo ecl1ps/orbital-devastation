a568d819dc8cce7b7323df5fe2e5884075adb5eb
hromada2 <jakub.hromadka@gmail.com>
2013-01-23 00:59:04 +0100

    kdyz hrac stiskne A vytvori par particlu ;) particles nesmi mit alfu (ani barvu s alpha channelem), bude chtit vyzkouset nahodit heavyweight objekty se shaderem na alphu jestli to nebude lepsi + particle controly updatovat na jinem vlakne

--- Application/Core/Players/Input/PlayerInputMgr.cs
+++ Application/Core/Players/Input/PlayerInputMgr.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Windows.Input;
 using System.Windows;
 using Orbit.Core.Client;
+using Orbit.Core.Scene.Particles.Implementations;
+using System.Windows.Media;
 
 namespace Orbit.Core.Players.Input
 {
@@ -15,6 +17,36 @@ namespace Orbit.Core.Players.Input
         public PlayerInputMgr(Player p, SceneMgr sceneMgr, ActionBarMgr actionMgr) : base(actionMgr, sceneMgr) 
         {
             plr = p;
+        }
+
+        public override void OnKeyEvent(KeyEventArgs e)
+        {
+            base.OnKeyEvent(e);
+            if (e.Key == Key.A)
+            {
+                ParticleEmmitor emmitor = new ParticleEmmitor(sceneMgr, IdMgr.GetNewId(plr.GetId()));
+                emmitor.EmitingTime = 10;
+                emmitor.EmmitingDirection = new Vector(1, 1);
+                emmitor.MinAngle = (float) -Math.PI;
+                emmitor.MaxAngle = (float) Math.PI;
+                emmitor.MinForce = 50;
+                emmitor.MaxForce = 70;
+                emmitor.MinLife = 1;
+                emmitor.MaxLife = 3;
+                emmitor.Position = new Vector(500, 500);
+                emmitor.MinSize = 2;
+                emmitor.MaxSize = 6;
+                emmitor.MaxAlpha = 0.6f;
+                emmitor.Amount = 5;
+                emmitor.FireAll = true;
+
+                ParticleSphereFactory f = new ParticleSphereFactory();
+                f.Color = Colors.Red;
+                emmitor.Factory = f;
+
+                sceneMgr.DelayedAttachToScene(emmitor);
+
+            }
         }
 
         public override void OnCanvasClick(Point point, MouseButtonEventArgs e)
