7ad49dfd2f0e4c4e7b51a449aff7e606f040cbb1
hromada2 <jakub.hromadka@gmail.com>
2013-01-25 01:03:17 +0100

    testovaci particle system ktery nahradi stavajici

--- Application/Core/Players/Input/PlayerInputMgr.cs
+++ Application/Core/Players/Input/PlayerInputMgr.cs
@@ -18,53 +18,6 @@ namespace Orbit.Core.Players.Input
         public PlayerInputMgr(Player p, SceneMgr sceneMgr, ActionBarMgr actionMgr) : base(actionMgr, sceneMgr) 
         {
             plr = p;
-
-            emmitor = new ParticleEmmitor(sceneMgr, IdMgr.GetNewId(plr.GetId()));
-            emmitor.EmitingTime = 2f;
-            emmitor.EmmitingDirection = new Vector(0, -1);
-            emmitor.MinAngle = (float)(-Math.PI / 20);
-            emmitor.MaxAngle = (float)(Math.PI / 20);
-            emmitor.MinForce = 30;
-            emmitor.MaxForce = 50;
-            emmitor.MinLife = 3f;
-            emmitor.MaxLife = 3.5f;
-            emmitor.Position = new Vector(500, 500);
-            emmitor.MinSize = 3;
-            emmitor.MaxSize = 4;
-            emmitor.MaxAlpha = 0.6f;
-            emmitor.Amount = 80;
-            emmitor.Infinite = true;
-            emmitor.Enabled = false;
-
-            ParticleSphereFactory f = new ParticleSphereFactory();
-            f.Color = Color.FromArgb(20, 0, 0, 0);
-            emmitor.Factory = f;
-
-            sceneMgr.DelayedAttachToScene(emmitor);
-
-            ParticleEmmitor e = new ParticleEmmitor(sceneMgr, IdMgr.GetNewId(plr.GetId()));
-            e.EmitingTime = 2f;
-            e.EmmitingDirection = new Vector(0, -1);
-            e.MinAngle = (float)(-Math.PI / 20);
-            e.MaxAngle = (float)(Math.PI / 20);
-            e.MinForce = 30;
-            e.MaxForce = 50;
-            e.MinLife = 3f;
-            e.MaxLife = 3.5f;
-            e.Position = new Vector(200, 500);
-            e.MinSize = 3;
-            e.MaxSize = 4;
-            e.MaxAlpha = 0.6f;
-            e.Amount = 100;
-            e.Infinite = true;
-            e.Enabled = false;
-
-
-            ParticleSphereFactory fa = new ParticleSphereFactory();
-            fa.Color = Color.FromArgb(20, 0, 0, 0);
-            e.Factory = fa;
-
-            sceneMgr.DelayedAttachToScene(e);
         }
 
         public override void OnKeyEvent(KeyEventArgs e)
