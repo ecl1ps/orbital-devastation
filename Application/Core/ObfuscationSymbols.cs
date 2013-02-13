e797831c9708d7110212bc768eddbfb84db5a3a0
Eclipse <Eclipse@something.com>
2013-02-13 22:52:16 +0100

    opraven produkcni build

--- Application/Core/ObfuscationSymbols.cs
+++ Application/Core/ObfuscationSymbols.cs
@@ -14,6 +14,7 @@ using System.Reflection;
 [assembly: Obfuscation(Feature = "embed log4net.dll", Exclude = false)]
 [assembly: Obfuscation(Feature = "embed WPFLocalizeExtension.dll", Exclude = false)]
 [assembly: Obfuscation(Feature = "embed XAMLMarkupExtensions.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "embed Petzold.Media3D.dll", Exclude = false)]
 
 // zapnuti debugovani v release
 //[assembly: Obfuscation(Feature = "debug", Exclude = false)]
--- Application/Strings.Designer.cs
+++ Application/Strings.Designer.cs
@@ -22,6 +22,7 @@ namespace Orbit {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [System.Reflection.ObfuscationAttribute(Feature = "renaming")]
     internal class Strings {
         
         private static global::System.Resources.ResourceManager resourceMan;
