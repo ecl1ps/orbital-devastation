07547537980c49a46c349ff2729d63c565525f75
EcliJen <eclipsejenkins@gmail.com>
2013-02-18 15:16:03 +0100

    logovani vsech vyjimek

--- MasterServer/Program.cs
+++ MasterServer/Program.cs
@@ -8,15 +8,25 @@ namespace MasterServer
 {
     static class Program
     {
+        private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainWindow());
+            }
+            catch (Exception e)
+            {
+                Logger.Fatal(e.StackTrace);
+                throw;
+            }
         }
     }
 }
