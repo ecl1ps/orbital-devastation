8a2cf276a5c6d2383bb71f6e8f83b7281da5a38c
hromada2 <jakub.hromadka@gmail.com>
2013-02-20 02:53:27 +0100

    emmitor prepisuje a nevola predkovo remove

--- Application/Core/Scene/Particles/Implementations/ParticleEmmitor.cs
+++ Application/Core/Scene/Particles/Implementations/ParticleEmmitor.cs
@@ -388,9 +388,11 @@ namespace Orbit.Core.Scene.Particles.Implementations
 
         public override void DoRemove(ISceneObject obj)
         {
-            base.DoRemove(obj);
             if (obj is ParticleEmmitor)
                 SceneMgr.RemoveParticleEmmitor(obj as ParticleEmmitor);
+
+            controls.ForEach(control => control.OnRemove());
+            controls.ForEach(control => control.OnSceneObjectRemove());
 
             particleArea.BeginInvoke(new Action(() => particleArea.WorldModels.Children.Remove(model)));
         }
