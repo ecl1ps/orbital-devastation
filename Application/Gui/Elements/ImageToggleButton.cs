c9cffde3d2c5e07e1412ab7e36f7ef73fc2f5aaf
Eclipse <Eclipse@something.com>
2013-02-28 13:32:47 +0100

    pridan element ImageToggleButton, ktery ma jiny styl nez klasicky ToggleButton a umi zobrazovat obrazek (lepsi zpusob jsem nenasel) - fixes #150: ToggleButton s obrazky

---
+++ Application/Gui/Elements/ImageToggleButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls.Primitives;
+
+namespace Orbit.Gui.Elements
+{
+    public class ImageToggleButton : ToggleButton
+    {
+
+    }
+}
