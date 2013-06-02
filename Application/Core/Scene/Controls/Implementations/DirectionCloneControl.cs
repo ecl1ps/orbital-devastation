62e3e4c7b0363a0af75b8609c1a10a7af144f01b
Eclipse <Eclipse@something.com>
2013-06-02 21:40:20 +0200

    controly pro klonovani direction a rotovani podle direction

---
+++ Application/Core/Scene/Controls/Implementations/DirectionCloneControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Orbit.Core.Scene.Entities;
+
+namespace Orbit.Core.Scene.Controls.Implementations
+{
+    public class DirectionCloneControl : Control
+    {
+        private IMovable cloned;
+        private IMovementControl control;
+
+        public DirectionCloneControl(ISceneObject cloned)
+        {
+            if (!(cloned is IMovable))
+                throw new ArgumentException("DirectionCloneControl must clone an IMovable object");
+
+            this.cloned = cloned as IMovable;
+        }
+
+        protected override void InitControl(ISceneObject me)
+        {
+            if (!(me is IMovable))
+                throw new ArgumentException("DirectionCloneControl must by attached to an IMovable object");
+
+            control = cloned.GetControlOfType<IMovementControl>();
+            if (control != null)
+                (me as IMovable).Direction = control.RealDirection;
+            else
+                (me as IMovable).Direction = cloned.Direction;
+        }
+
+        protected override void UpdateControl(float tpf)
+        {
+            control = cloned.GetControlOfType<IMovementControl>();
+            if (control != null)
+                (me as IMovable).Direction = control.RealDirection;
+            else
+                (me as IMovable).Direction = cloned.Direction;
+        }
+    }
+}
---
+++ Application/Core/Scene/Controls/Implementations/DirectionDrivenRotationControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Orbit.Core.Scene.Entities;
+
+namespace Orbit.Core.Scene.Controls.Implementations
+{
+    public class DirectionDrivenRotationControl : Control
+    {
+        private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected override void InitControl(ISceneObject me)
+        {
+            if (!(me is IRotable) || !(me is IMovable))
+                throw new ArgumentException("DirectionDrivenRotationControl must by attached to an IRotable and an IMovable object");
+        }
+
+        protected override void UpdateControl(float tpf)
+        {
+            (me as IRotable).Rotation = (float)FastMath.DegToRad(Vector.AngleBetween(SharedDef.DEFAULT_VECTOR, (me as IMovable).Direction));
+
+            //Logger.Debug("Rotation: " + (me as IRotable).Rotation + " vec(" + (me as IMovable).Direction.ToString() + ")");
+        }
+    }
+}
