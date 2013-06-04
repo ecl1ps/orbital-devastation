b4efa3c9ba308aaa995d32f84ec3e1801711d7f3
Eclipse <Eclipse@something.com>
2013-06-05 00:37:14 +0200

    PositionCloneControl umi pouzivat offset

--- Application/Core/Scene/Controls/Implementations/PositionCloneControl.cs
+++ Application/Core/Scene/Controls/Implementations/PositionCloneControl.cs
@@ -9,21 +9,24 @@ namespace Orbit.Core.Scene.Controls.Implementations
 {
     public class PositionCloneControl : Control
     {
+        public Vector Offset { get; set; }
+
         private ISceneObject toFollow;
 
         public PositionCloneControl(ISceneObject toFollow)
         {
+            Offset = new Vector(0, 0);
             this.toFollow = toFollow;
         }
 
         protected override void InitControl(ISceneObject me)
         {
-            me.Position = toFollow.Position;
+            me.Position = toFollow.Position - Offset;
         }
 
         protected override void UpdateControl(float tpf)
         {
-            me.Position = toFollow.Position;
+            me.Position = toFollow.Position - Offset;
         }
     }
 }
